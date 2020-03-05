3d2571da14be3a640ed562d624586cd328188b18
Jose Carlos Ramirez Moreno <jramirezm@aic.pgjguanajuato.gob.mx>
2020-03-05 15:36:04 -0600

    Video 189  + Colección de Tickets pendientes de atender

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -1,16 +1,26 @@
 const fs = require('fs');
 
+class Ticket {
+
+    constructor(numero, escritorio) {
+        this.numero = numero;
+        this.escritorio = escritorio;
+    }
+}
+
 class TicketControl {
 
     constructor() {
 
         this.ultimo = 0;
         this.hoy = new Date().getDate();
+        this.tickets = [];
 
         let data = require('../data/data.json');
 
         if ( data.hoy === this.hoy ) {
             this.ultimo = data.ultimo;
+            this.tickets = data.tickets;
         } else {
             this.reiniciarConteo();
         }
@@ -20,6 +30,9 @@ class TicketControl {
     siguiente() {
 
         this.ultimo += 1;
+        let ticket = new Ticket(this.ultimo, null);
+        this.tickets.push(ticket);
+
         this.grabarArchivo();
 
         return `Ticket ${ this.ultimo }`;
@@ -32,6 +45,7 @@ class TicketControl {
     reiniciarConteo() {
 
         this.ultimo = 0;
+        this.tickets = [];
         console.log('Se ha inicializado el sistema');
         this.grabarArchivo();
     }
@@ -40,7 +54,8 @@ class TicketControl {
 
         let jsonData = {
             ultimo: this.ultimo,
-            hoy: this.hoy
+            hoy: this.hoy,
+            tickets: this.tickets
         }
 
         let jsonDataString = JSON.stringify(jsonData);
